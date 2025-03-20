fa2dcde09b34502e5fcbf1aa3e9c675c031846d0
Pabitra.kr chalise <pabitrakumarchalise@gmail.com>
2025-03-20 08:45:40 +0545

    Add files via upload

---
+++ Day4.py
@@ -0,0 +1,19 @@
+import requests
+from tabulate import tabulate
+city_name="Lalitpur"
+api_key="8578b0e1d22b100909c7a832bfa2e0f7"
+base_url=f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric"
+
+respone=requests.get(base_url)
+if respone.status_code == 200:
+    print('Status ok')
+
+    data=respone.json()
+    weather=(data['weather'][0]['description'])#mist
+    temperature=(data['main']['temp'])
+    feelslike=(data['main']['feels_like'])
+    humidity=(data['main']['humidity'])
+
+    mweather=[[humidity,feelslike,weather,temperature]]
+    head=["humidity","feels like(°C)","Weather","temperature(°C)"]
+    print(tabulate(mweather,headers=head,tablefmt='grid'))

c76cd306e7478ada3e6a7e12ee57dd8c2d05626f
Pabitra.kr chalise <pabitrakumarchalise@gmail.com>
2025-04-02 20:52:38 +0545

    Add files via upload

---
+++ Day17.py
@@ -0,0 +1,11 @@
+import io
+import random
+import string
+import warnings
+import numpy
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import warnings
+warnings.filterwarnings('ignore')
+import nltk
+
